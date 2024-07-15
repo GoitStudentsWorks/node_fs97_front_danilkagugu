dce259ec66b6e45e03e738ef064f73d609f54b0e
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-07-15 08:09:50 +0000

    Deploying to gh-pages from @ GoitStudentsWorks/node_fs97_front_danilkagugu@73a3d3b0b8372de771f9240726da52a62d5a7c80 🚀

---
+++ assets/Logo-DYcVhLjl.js
@@ -0,0 +1,2 @@
+import{j as o,L as s}from"./index-CvA_--r5.js";const t="_logo_bgrtb_1",r={logo:t},c=()=>o.jsx(s,{to:"/",className:r.logo,children:"AquaTrack"});export{c as L};
+//# sourceMappingURL=Logo-DYcVhLjl.js.map
